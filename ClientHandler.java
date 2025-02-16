175cd8402982ee563c575b288378b8ab9c2c66ff
judychickadee <judyabed22@gmail.com>
2025-02-16 10:57:46 +0300

    ClientHandler Start

---
+++ ClientHandler.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.List;
+
+public class ClientHandler implements Runnable{
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String playerName;
+
+    private List<ClientHandler> players;
+    private static List<ClientHandler> waitingRoom;
+
+    public ClientHandler(Socket socket, List<ClientHandler> players, List<ClientHandler> waitingRoom){
+        this.socket = socket;
+        this players = players;
+        ClientHandler.waitingRoom = waitingRoom;
+    }
+    
+
+    @Override
+    public void run() {
+        try {
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            out.flush();
+
+            
+
+            // Add an if statement for invalid player namesp
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
