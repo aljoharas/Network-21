154966d9a3f2382ae924ba7fe4e1fbb7c8511208
aljoharas <alsultanaljohara04@Gmail.com>
2025-02-16 10:25:15 +0300

    cards + deck

---
+++ Card.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Card {
+    String suit;
+    String rank;
+    int value;
+
+    public Card(String suit, String rank, int value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
