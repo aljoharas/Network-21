98d3d28a75ea3ff069a22e956079fa701eaa2367
DanaZaid5 <danaalsalami2003@gmail.com>
2025-02-18 12:01:56 +0300

    ddd

---
+++ prohect/src/main/java/com/mycompany/prohect/Prohect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prohect;
+
+/**
+ *
+ * @author danaalsalami
+ */
+public class Prohect {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
