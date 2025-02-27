db543f2056a6e42433314c9652d77d0fdea1e7c5
aljoharas <alsultanaljohara04@Gmail.com>
2025-02-27 21:50:12 +0300

    gui

--- network21/ServerListener.java
+++ network21/ServerListener.java
@@ -4,24 +4,32 @@ import java.net.Socket;
 import java.io.*;
 
 class ServerListener implements Runnable {
-    
+
     private Socket socket;
     private BufferedReader in;
+    private Player player;
 
-    public ServerListener(Socket socket) throws IOException {
+    public ServerListener(Socket socket, Player player) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.player = player;
+
     }
 
     @Override
     public void run() {
         try {
             String serverMessage;
+            System.out.println("Listening");
+            serverMessage = in.readLine();
+            System.out.println(serverMessage);
             while ((serverMessage = in.readLine()) != null) {
                 if (serverMessage.trim().isEmpty()){
                     continue;
                 }
-                System.out.println("Server: " + serverMessage); // Print server messages
+                System.out.println("Received from server: " + serverMessage);
+                player.handleMessage(serverMessage);
+                //System.out.println("Server: " + serverMessage);
             }
         } catch (IOException e) {
             e.printStackTrace();
